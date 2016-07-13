db7c0044716846541da6e6a77ae1dd412cdee2a3
darkenvy <darkenvy6@gmail.com>
2016-07-12 17:33:57 -0700

    Added a few more rotues

--- app/controllers/ranger_controller.rb
+++ app/controllers/ranger_controller.rb
@@ -3,9 +3,34 @@ class RangerController < ApplicationController
     @rangers = Ranger.all
   end
 
+  def new
+    @ranger = Ranger.new
+  end
+
+  def create
+    ranger = Ranger.new(ranger_params)
+    if ranger
+      flash[:success] = "Created a new ranger"
+      redirect_to ranger_index_path
+    else
+      flash[:danger] = "Failed to create a new ranger"
+      redirect_to new_ranger
+    end
+  end
+
+  def show
+    @ranger = Ranger.find(params[:id])
+  end
+
   def destroy
     d = Ranger.find(params[:id])
     d.destroy
     redirect_to ranger_index_path
+  end
+
+  private
+
+  def ranger_params
+    params.require(:ranger).permit(:name, :years)
   end
 end
