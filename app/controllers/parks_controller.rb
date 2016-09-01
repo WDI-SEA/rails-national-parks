983fe7db5612d799b1857ae1bb9431fd987caf87
kedeger <kedeger@uw.edu>
2016-09-01 13:11:20 -0700

    app set up

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,25 @@
+class ParksController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroycd
+  end
+
+  def ..
+  end
+end
