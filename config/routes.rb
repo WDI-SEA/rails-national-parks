7ca5f06e9b86909cb772bd6750ea35437706c8f4
britbroet <brittanybroetje@gmail.com>
2016-11-16 08:56:03 -0800

    national parks stuff crud working cloudinary fail

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  get 'parks/index'
+
+  get 'parks/new'
+
+  get 'parks/edit'
+
+  get 'parks/show'
+
+  root 'main#index'
+
+  resources :parks
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
