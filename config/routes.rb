76aface77fe2226bda0643ffe32fe47171900103
Hailz <hailey083@yahoo.com>
2017-04-04 21:39:09 -0700

    Needs styling but everything works

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'parks#index'
+
+  get 'parks/index'
+
+  get 'parks/new'
+
+  get 'parks/edit'
+
+  get 'parks/:id' => 'parks#show'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :parks
+  
+end
