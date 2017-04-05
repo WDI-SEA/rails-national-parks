0e6daa63c3d3b4fbb93e402f1e4fd30cd89c5563
krichdev <kgrichardson2@gmail.com>
2017-04-05 09:10:09 -0700

    finished part 1 for deliverable

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,33 @@
+class ParksController < ApplicationController
+  def index
+    @parks = Park.all
+  end
+
+  def new
+    @park = Park.new
+  end
+
+  def create
+    Park.create(park_params)
+    redirect_to parks_path
+  end
+
+  def edit
+    @park = Park.find(params[:id]).update(park_params)
+    redirect_to parks_path
+  end
+
+  def show
+    @park = Park.find(params[:id])
+  end
+
+  def destory
+    Park.find(params[:id]).delet
+    redirect_to parks_path
+  end
+
+  def park_params
+    params.require(:park).permit(:name, :description, :picture)
+  end
+  
+end
