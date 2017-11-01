4d28663e44d55c62165bb86475b5f43cee59706e
laurenperez <laurenashleymarks@gmail.com>
2017-10-31 23:20:01 -0700

    first half complete

---
+++ app/controllers/parks_controller.rb
@@ -0,0 +1,41 @@
+class ParksController < ApplicationController
+
+  def index
+    @parks = Park.all
+  end
+
+  def new
+    @park = Park.new
+  end
+
+  def create
+    Park.create(park_params)
+    redirect_to parks_path
+  end
+
+  def show
+    @park = Park.find(params[:id])
+  end
+
+  def edit
+    @park = Park.find(params[:name])
+  end
+
+  def update
+    p = Park.find(params[:id])
+    p.update(park_params)
+    redirect_to parks_path
+  end
+
+  def destroy
+    Park.find(params[:id]).delete
+    redirect_to parks_path
+  end
+
+  private
+  #this is used to prevent mass-assignment of params to active record models
+  def park_params
+    params.require(:park).permit(:name, :description)
+  end
+
+end
